620bcc18d5be09249ece087c776068c05e796f2c
GabrielGrenier <gabriel.braun-grenier@hotmail.com>
2022-09-13 22:22:34 -0400

    toast + value validation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,9 +13,9 @@ function Home() {
     const [guesses, setGuesses] = useState<guess[]>([]);
     const currentColor:guess = {red:255, green:160, blue: 100};
 
-    const [red, setRed] = useState<number|undefined>(undefined);
-    const [green, setGreen] = useState<number|undefined>(undefined);
-    const [blue, setBlue] = useState<number|undefined>(undefined);
+    const [red, setRed] = useState<number>(9999);
+    const [green, setGreen] = useState<number>(9999);
+    const [blue, setBlue] = useState<number>(9999);
 
     useEffect(() => {
         //setGuesses();
@@ -24,10 +24,14 @@ function Home() {
     const makeGuess = () => {
         var arrTemp = [...guesses];
         
-        if(red !== undefined && green !== undefined && blue !== undefined){
+        if(red <= 255 && red >= 0 && green <= 255 && green >= 0 && blue <= 255 && blue >= 0){
             arrTemp.push({red: red, green: green, blue: blue});
-
             setGuesses(arrTemp);
+        }
+        else {
+            toast.error("Color values must be between 0 and 255.", {
+                theme: 'dark'
+            });
         }
     }
 
@@ -60,6 +64,18 @@ function Home() {
     }
 
     return <>
+        <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+        />
+        
         {/* Header */}
         <div className="px-5 py-5 border-b-2 border-neutral-700">
             <div className="float-right w-10 h-10 pt-0.5 text-3xl text-center rounded-full bg-neutral-600 hover:cursor-pointer hover:bg-neutral-300 transition duration-300">
@@ -78,30 +94,31 @@ function Home() {
             <div className="w-full p-2 mt-2 border-2 rounded-lg border-neutral-600">
                 <div className="grid grid-cols-4 gap-4">
                     <div>
-                        <input type="number" placeholder="Red" className="w-full px-2 py-1 text-white placeholder-white bg-red-500 rounded-full" onChange={(e) => setRed(Number(e.target.value))} value={red}/>
+                        <input type="number" placeholder="Red" className="w-full px-2 py-1 text-white placeholder-white bg-red-500 rounded-full" onChange={(e) => setRed(Number(e.target.value))} />
                     </div>
                     <div>
-                        <input type="number" placeholder="Green" className="w-full px-2 py-1 text-white placeholder-white bg-green-500 rounded-full" onChange={(e) => setGreen(Number(e.target.value))} value={green}/>
+                        <input type="number" placeholder="Green" className="w-full px-2 py-1 text-white placeholder-white bg-green-500 rounded-full" onChange={(e) => setGreen(Number(e.target.value))} />
                     </div>
                     <div>
-                        <input type="number" placeholder="Blue" className="w-full px-2 py-1 text-white placeholder-white bg-blue-500 rounded-full" onChange={(e) => setBlue(Number(e.target.value))} value={blue}/>
+                        <input type="number" placeholder="Blue" className="w-full px-2 py-1 text-white placeholder-white bg-blue-500 rounded-full" onChange={(e) => setBlue(Number(e.target.value))} />
                     </div>
                     <div>
-                        <button className="w-full px-2 py-1 transition duration-300 rounded-full bg-neutral-500 hover:bg-neutral-400" onClick={() => makeGuess()}>Guess</button>
+                        <button className="w-full px-2 py-1 transition duration-300 rounded-full bg-neutral-500 hover:bg-neutral-400 disabled:bg-neutral-700 disabled:text-neutral-500 disabled:cursor-not-allowed" onClick={() => makeGuess()} disabled={guesses.length >= 8}>
+                            {guesses.length >= 8 ? "😞" : "Guess"}
+                        </button>
                     </div>
                 </div>
             </div>
 
             {guesses.length > 0 && <div className="grid grid-cols-4 gap-4 px-2 pt-10 text-center">
-                    <p>Red</p>
-                    <p>Green</p>
-                    <p>Blue</p>
-                    <p>Tries</p>
-                </div>
-            }
+                <p>Red</p>
+                <p>Green</p>
+                <p>Blue</p>
+                <p>Tries</p>
+            </div>}
 
             {/* Guesses */}
-            {guesses.map((guess, index) => <div className={`w-full p-2 border-2 rounded-lg border-neutral-600 ${index === 0 ? "" : "mt-5"}`}>
+            {guesses.map((guess, index) => <div className={`w-full p-2 border-2 rounded-lg border-neutral-600 ${index === 0 ? "" : "mt-5"}`} key={index}>
                 <div className="grid grid-cols-4 gap-4">
                     <div>
                         <div className={`w-full py-0.5 border-2 rounded-full  ${getBorderClass(guess.red, "red")}`}>
